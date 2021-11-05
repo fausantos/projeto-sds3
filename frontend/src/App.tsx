5fb5b540a391e367abea4eb26283a3a185944881
Flávio Santos <flaviovansantos@gmail.com>
2021-11-05 20:06:36 -0300

    Basic static components
    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+
+
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-    </div>
-    );
+    <>
+      <NavBar></NavBar>
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+
+        <DataTable></DataTable>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/fausantos" target="_blank" rel="noreferrer">Flávio Santos</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/flaviovansantos" target="_blank" rel="noreferrer">@FlavioVanSantos</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
